ac2e44fab3a274a2d20b93aea2c28849a9e76902
pitlinz <p.krebs@w4c.at>
2015-02-11 21:20:26 +0100

    predefined templates

---
+++ manifests/predefined/checkapache.pp
@@ -0,0 +1,54 @@
+/**
+ * predifined monit checks for apache2
+ * (c) 2015 by Peter Krebs <pitlinz@sourceforge.net> 
+ */
+define monit::predefined::checkapache(
+  $ensure       = present,
+  $conffile     = '',
+  $pidfile      = false,
+  $start        = "/etc/init.d/apache2 start",
+  $stop         = "/etc/init.d/apache2 stop",
+  $depends_on   = [],
+  $customlines  = [],
+  
+  $checkhost    = "localhost",
+  $checkrequest = '/',
+  
+) {
+
+  if $conffile != '' {
+    $depends_on_conf = "apacheconf_${name}"
+    monit::check::file{$depends_on_conf:
+      ensure    => $ensure,
+      filepath  => $conffile,      
+      before    => File["/etc/monit/conf.d/process_apache_${name}.conf"] 
+	  }	  
+  } else {
+    $depends_on_conf = false
+  }
+  
+  $processname = "${name}"
+  
+  if $pidfile {
+    $apachepidfile = "${pidfile}"
+  } else {
+    case $::lsbdistid {
+      'Debian': {
+          $apachepidfile = "/var/run/apache2.pid"
+        }
+      'Ubuntu': {
+          $apachepidfile = "/var/run/apache2/apache2.pid"
+        }
+    }
+  }
+  
+  file {"/etc/monit/conf.d/process_apache_${name}.conf":
+    ensure  => $ensure,
+    owner   => "root",
+    group   => "root",
+    mode    => "0400",
+    content => template("monit/predefined/check_process_apache.monitrc.erb"),    
+    notify  => Service["monit"],
+  }  
+  
+}
