0f48b334f0c39e1f5d4a13726369a5b13ea38ab6
pitlinz <p.krebs@w4c.at>
2015-03-26 21:52:53 +0100

    worksave updatemi

--- manifests/predefined/checkmdadm.pp
+++ manifests/predefined/checkmdadm.pp
@@ -16,43 +16,43 @@
 #   - Package["monit"]
 #
 define monit::predefined::checkmdadm(
-  $ensure       =present,
-  $warnlimit    =1,
-  $critlimit    =5,
-  $run_cnt_chk  = true, # runs mismatch_cnt script with param *
+	$ensure       =present,
+	$warnlimit    =1,
+	$critlimit    =5,
+	$run_cnt_chk  = true, # runs mismatch_cnt script with param *
 ) {
 
-  monit::check::file{"mdadm_conf":
-    ensure => $ensure,
-    filepath => "/etc/mdadm/mdadm.conf",
-    customlines => ["if failed checksum then alert"],
-  }
+	monit::check::file{"mdadm_conf":
+		ensure => $ensure,
+		filepath => "/etc/mdadm/mdadm.conf",
+		customlines => ["if failed checksum then alert"],
+	}
 
-  monit::check::process{"mdadm":
-    ensure => $ensure,
-    pidfile => "/var/run/mdadm/monitor.pid",
-    depends_on => ["mdadm_conf"],
-    customlines => ["if 2 restarts within 4 cycles then timeout"],
-  }
-  
-  if $run_cnt_chk { 
-    if !defined(File["/etc/monit/scripts/check-mdstat.sh"]) {
-		  file { "/etc/monit/scripts/check-mdstat.sh":
-		    ensure  => $ensure,
-		    owner   => "root",
-		    group   => "root",
-		    mode    => 0555,
-		    content => template("monit/checkscripts/check_mdstat.sh.erb"),
-		  }
-    }
-    
-	  monit::check::programm {"mdadm_check":
-	    ensure      => $ensure,
-	    scriptpath  => "/etc/monit/scripts/check-mdstat.sh",
-	    depends_on  => ["mdadm"],
-	    customlines => ["if status != 0 then alert"],
-	    require     => File["/etc/monit/scripts/check-mdstat.sh"],
-	  }
-  }
+	monit::check::process{"mdadm":
+		ensure => $ensure,
+		pidfile => "/var/run/mdadm/monitor.pid",
+		depends_on => ["mdadm_conf"],
+		customlines => ["if 2 restarts within 4 cycles then timeout"],
+	}
+
+	if $run_cnt_chk {
+		if !defined(File["/etc/monit/scripts/check-mdstat.sh"]) {
+			file { "/etc/monit/scripts/check-mdstat.sh":
+				ensure  => $ensure,
+				owner   => "root",
+				group   => "root",
+				mode    => '0555',
+				content => template("monit/checkscripts/check_mdstat.sh.erb"),
+			}
+		}
+
+		monit::check::programm {"mdadm_check":
+			ensure      => $ensure,
+			scriptpath  => "/etc/monit/scripts/check-mdstat.sh",
+			depends_on  => ["mdadm"],
+			customlines => ["if status != 0 then alert"],
+			require     => File["/etc/monit/scripts/check-mdstat.sh"],
+		}
+  	}
 
 }
--- manifests/predefined/checkmysqld.pp
+++ manifests/predefined/checkmysqld.pp
@@ -7,22 +7,24 @@
 # Actions:
 #   The following actions gets taken by this defined type:
 define monit::predefined::checkmysqld(
-  $ensure=present,
-  $port="3306",
+	$ensure=present,
+	$port="3306",
 ) {
 
-  monit::check::file{"mysqld_conf":
-    filepath => "/etc/mysql/my.cnf",
-    customlines => ["if failed checksum then alert"],
-  }
+	monit::check::file{"mysqld_conf":
+		filepath => "/etc/mysql/my.cnf",
+		customlines => ["if failed checksum then alert"],
+	}
 
-  monit::check::process{"mysqld":
-    pidfile     => "/var/run/mysqld/mysqld.pid",
-    start       => "/usr/sbin/service mysql start",
-    stop        => "/usr/sbin/service mysql stop",
-    depends_on  => ["mysqld_conf"],
-    customlines => ["if failed port ${port} then restart",
-                    "if 2 restarts within 3 cycles then timeout"]
+	monit::check::process{"mysqld":
+		pidfile     => "/var/run/mysqld/mysqld.pid",
+		start       => "/usr/sbin/service mysql start",
+		stop        => "/usr/sbin/service mysql stop",
+		depends_on  => ["mysqld_conf"],
+		customlines => [
+			"if failed port ${port} then restart",
+           	"if 2 restarts within 3 cycles then timeout"
+       	]
   }
 
 }
